154f1f47da49f9210f964c7948fc3ebf43808620
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-10-24 12:57:17 -0300

    Trabs

--- ElementosInterface/app/src/main/java/com/example/elementosinterface/MainActivity.java
+++ ElementosInterface/app/src/main/java/com/example/elementosinterface/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.elementosinterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextRoupa;
+    private TextView textViewEscolha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void mudaTexto (View view){
+        editTextRoupa=findViewById(R.id.editTextRoupa);
+        textViewEscolha=findViewById(R.id.textViewEscolha);
+
+        textViewEscolha.setText("Sua Escolha: "+editTextRoupa.getText());
+    }
+
+
 }
