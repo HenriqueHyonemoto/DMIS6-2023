f76a84ac212915c528913609d8cf81e3a7a58450
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-10-24 13:32:54 -0300

    final

--- ElementosInterface/app/src/main/java/com/example/elementosinterface/MainActivity.java
+++ ElementosInterface/app/src/main/java/com/example/elementosinterface/MainActivity.java
@@ -4,24 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private EditText editTextRoupa;
-    private TextView textViewEscolha;
+    private TextView textViewEscolha, textViewCores;
+    private CheckBox checkBoxBranco, checkBoxVerde, checkBoxAzul;
 
+    List<String> checked = new ArrayList<String>();
+
+    public void getTexto(View view){
+        textViewEscolha.setText(editTextRoupa.getText());
+        verificaCheck(view);
+    }
+
+    public void verificaCheck(View view){
+        checked.clear();
+        textViewCores = findViewById(R.id.textViewCores);
+
+        if(checkBoxBranco.isChecked()){
+            checked.add(checkBoxBranco.getText().toString());
+        }
+        if(checkBoxVerde.isChecked()){
+            checked.add(checkBoxVerde.getText().toString());
+        }
+        if(checkBoxAzul.isChecked()){
+            checked.add(checkBoxAzul.getText().toString());
+        }
+        textViewCores.setText(checked.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
     public void mudaTexto (View view){
         editTextRoupa=findViewById(R.id.editTextRoupa);
         textViewEscolha=findViewById(R.id.textViewEscolha);
+        checkBoxBranco=findViewById(R.id.checkBoxBranco);
+        checkBoxVerde=findViewById(R.id.checkBoxVerde);
+        checkBoxAzul=findViewById(R.id.checkBoxAzul);
 
         textViewEscolha.setText("Sua Escolha: "+editTextRoupa.getText());
+        verificaCheck(view);
     }
 
 
