1e6fee89f73596eb1be5d22ef12b8e42c2b0b8d8
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-10-24 12:11:58 -0300

    mgdls

---
+++ Scroll242023/app/src/main/java/com/example/scroll24_20_23/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scroll24_20_23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
