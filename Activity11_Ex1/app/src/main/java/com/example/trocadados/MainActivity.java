e117e98833c9a4a2d68aba5cd378183c6d37d0cb
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-12-05 10:58:10 -0300

    ex1completo

--- Activity11_Ex1/app/src/main/java/com/example/trocadados/MainActivity.java
+++ Activity11_Ex1/app/src/main/java/com/example/trocadados/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+
+    private EditText v1Input, v2Input, v3Input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
         button=findViewById(R.id.button);
 
+        v1Input = findViewById(R.id.v1Input);
+        v2Input = findViewById(R.id.v2Input);
+        v3Input = findViewById(R.id.v3Input);
+
+
         button.setOnClickListener(new View.OnClickListener(){
                                       @Override
                                       public void onClick(View v){
 Intent intent=new Intent(getApplicationContext(),RecebeDados.class);
-
-intent.putExtra("v1",1);
-intent.putExtra("v2",2);
-intent.putExtra("v3",3);
+                                          int v1Int,v2Int,v3Int;
+                                          v1Int = Integer.parseInt(v1Input.getText().toString());
+                                          v2Int = Integer.parseInt(v2Input.getText().toString());
+                                          v3Int = Integer.parseInt(v3Input.getText().toString());
+intent.putExtra("v1",v1Int);
+intent.putExtra("v2",v2Int);
+intent.putExtra("v3",v3Int);
 startActivity(intent);                               }
         }
 
