7cdba0e2dd3ff2bb9560922e2079048f42ffafd2
HenriqueHyonemoto <henrique.hyonemoto@aluno.ifsp.edu.br>
2023-12-05 12:18:10 -0300

    LIXO

---
+++ Activity11_Ex3/app/src/main/java/com/example/trocadados/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.trocadados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private EditText v1Input, v2Input, v3Input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+
+        v1Input = findViewById(R.id.v1Input);
+        v2Input = findViewById(R.id.v2Input);
+        v3Input = findViewById(R.id.v3Input);
+
+
+        button.setOnClickListener(new View.OnClickListener(){
+                                      @Override
+                                      public void onClick(View v){
+Intent intent=new Intent(getApplicationContext(),RecebeDados.class);
+                                          int v1Int,v2Int,v3Int;
+                                          v1Int = Integer.parseInt(v1Input.getText().toString());
+                                           v2Int = Integer.parseInt(v2Input.getText().toString());
+                                          v3Int = Integer.parseInt(v3Input.getText().toString());
+intent.putExtra("v1",v1Int);
+intent.putExtra("v2",v2Int);
+intent.putExtra("v3",v3Int);
+startActivity(intent);                               }
+        }
+
+
+
+        );
+    }
+}
---
+++ Activity11_Ex3/app/src/main/java/com/example/trocadados/RecebeDados.java
@@ -0,0 +1,37 @@
+package com.example.trocadados;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RecebeDados extends AppCompatActivity {
+private TextView txtV1,txtV2,txtV3, txtTriangulo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_dados);
+
+        txtV1 = findViewById(R.id.textViewV1);
+        txtV2 = findViewById(R.id.textViewV2);
+        txtV3 = findViewById(R.id.textViewV3);
+        txtTriangulo = findViewById(R.id.textViewTriangulo);
+
+        int v1,v2,v3;
+        String tipoTriangulo;
+
+        Bundle dados=getIntent().getExtras();
+
+        v1=dados.getInt("v1");
+        v2=dados.getInt("v2");
+        v3=dados.getInt("v3");
+        tipoTriangulo=dados.getString("tipoTriangulo");
+
+        txtV1.setText("Lado1: "+v1);
+        txtV2.setText("Lado2: "+v2);
+        txtV3.setText("Lado3: "+v3);
+        txtTriangulo.setText("Tipo do Triangulo: "+tipoTriangulo);
+
+    }
+}
